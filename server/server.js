1ac04111b5d7ab3e37d040f4bc2046b964224edc
Saad Siddiqui <saadsiddiqui-programmer@protonmail.com>
2023-10-23 12:30:11 -0700

    extended express encoding

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ const connectDB = async () => {
 
 app.use(cors());
 app.use(cookieParser());
-app.use(express.urlencoded({ limit: "100mb" }));
+app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 
 app.use(express.json({ limit: "100mb" }));
 
