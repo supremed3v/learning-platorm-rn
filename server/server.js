29c4c7a7ad776926ac7a835ae011d2a6bfad6cf1
Saad Siddiqui <saadsiddiqui-programmer@protonmail.com>
2023-10-23 13:09:52 -0700

    added cors origin

--- server/server.js
+++ server/server.js
@@ -22,7 +22,12 @@ const connectDB = async () => {
   }
 };
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 
